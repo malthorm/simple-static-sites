66e600244cd6e6ddac7b8a58ec54c4b44e08b2fc
Malte Thormählen <malte.thormaehlen@vivavis.com>
2026-01-27 13:42:46 +0100

    CH2, L2: add tests for TestNode

---
+++ src/test_textnode.py
@@ -0,0 +1,36 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_with_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, None)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_with_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        node2 = TextNode(
+            "This is a text node", TextType.BOLD, "https://www.example.com"
+        )
+        self.assertNotEqual(node, node2)
+
+    def test_text_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a another text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_type_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
